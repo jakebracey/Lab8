d3d55e844d430292d66b78132d5541c56b654190
jakebracey <jakebracey62@gmail.com>
2016-10-27 11:11:20 -0500

    got started on the code

--- l8.cpp
+++ l8.cpp
@@ -15,3 +15,54 @@
 
 
 using namespace std;
+
+class MSG{
+	protected:
+		string msg;
+		
+	public:
+		MSG();
+		MSG(string);
+		~MSG();
+		virtual void printInfo();
+};
+
+class mcMSG : public MSG{
+	public:
+		string * tran_msg;
+		int index;
+		void printInfo();
+		void translate();
+	
+};
+
+class MSGSt{
+	public:
+		MSG *ptrSt[10];
+		int st_top_ptr;
+		int num_obj;
+		//may need more
+		
+		void push(MSG obj);
+		void pop();
+		void printStack();
+};
+
+void MSGSt::push(MSG obj){
+	ptrSt[st_top_ptr]=new MSG(obj);
+	st_top_ptr++;
+}
+
+void MSGSt::pop(){
+	delete ptrSt[st_top_ptr];
+}
+
+
+int main(void){
+	
+	//put these into the translate function
+	string letters="abcdefghijklmnopqrstuvwxyz"
+	string morseCode[]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+	       "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
+	       "..-","...-",".--","-..-","-.--","--.."};
+}
