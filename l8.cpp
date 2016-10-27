1dee797bd3d6a76a47a280f5818b21905efb6cf1
jakebracey <jakebracey62@gmail.com>
2016-10-27 10:01:46 -0500

    initial commit

---
+++ l8.cpp
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : L7.cpp
+ Author      : Jacob Bracey
+ Description : ECE 3220: Lab 7
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <iostream>
+#include <cstring>
+
+
+using namespace std;
